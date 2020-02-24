4debacbc777d27b12a0b8fd89ad525a70a15a32e
Neil Lake <neil.1475369@gmail.com>
2020-02-24 11:29:37 +0000

    updated sticker

--- site/templates/_tickets.php
+++ site/templates/_tickets.php
@@ -1,80 +1,77 @@
-<div class="Event">
-    <div class="TicketsOnSaleWrapper">
-        <a href="#0">
-            <div class="TicketsOnSale">
-                Tickets. On&nbsp;sale. <span class="text-white">Now.</span>
-            </div>
-        </a>
-    </div>
+<div class="o-sticker">
+    <a href="#0">
+        <div class="o-sticker__stickerText">
+            Tickets. On&nbsp;sale. <span>Now.</span>
+        </div>
+    </a>
+</div>
 
-
-    <h1 class="">Lineup</h1>
-    <div class="">
-        <section class="">
-            <h2>Drinks.</h2>
-            <ul>
-                <li>Beavertown</li>
-                <li>BrewDog</li>
-                <li>Verdant</li>
-                <li>The Kernel</li>
-                <li>Cloudwater</li>
-                <li>Nort</li>
-            </ul>
-        </section>
-        <section class="">
-            <h2>Music.</h2>
-            <ul>
-                <li>Beavertown</li>
-                <li>BrewDog</li>
-                <li>Verdant</li>
-                <li>The Kernel</li>
-                <li>Cloudwater</li>
-                <li>Nort</li>
-            </ul>
-        </section>
-        <section class="">
-            <h2>Food.</h2>
-            <ul>
-                <li>Beavertown</li>
-                <li>BrewDog</li>
-                <li>Verdant</li>
-                <li>The Kernel</li>
-                <li>Cloudwater</li>
-                <li>Nort</li>
-            </ul>
-        </section>
-        <section class="">
-            <h2>Good Stuff.</h2>
-            <ul>
-                <li>Beavertown</li>
-                <li>BrewDog</li>
-                <li>Verdant</li>
-                <li>The Kernel</li>
-                <li>Cloudwater</li>
-                <li>Nort</li>
-            </ul>
-        </section>
-        <section class="">
-            <h2>Family.</h2>
-            <ul>
-                <li>Beavertown</li>
-                <li>BrewDog</li>
-                <li>Verdant</li>
-                <li>The Kernel</li>
-                <li>Cloudwater</li>
-                <li>Nort</li>
-            </ul>
-        </section>
-        <section class="">
-            <h2>Wellness.</h2>
-            <ul>
-                <li>Beavertown</li>
-                <li>BrewDog</li>
-                <li>Verdant</li>
-                <li>The Kernel</li>
-                <li>Cloudwater</li>
-                <li>Nort</li>
-            </ul>
-        </section>
-    </div>
+<h1 class="">Lineup</h1>
+<div class="">
+    <section class="">
+        <h2>Drinks.</h2>
+        <ul>
+            <li>Beavertown</li>
+            <li>BrewDog</li>
+            <li>Verdant</li>
+            <li>The Kernel</li>
+            <li>Cloudwater</li>
+            <li>Nort</li>
+        </ul>
+    </section>
+    <section class="">
+        <h2>Music.</h2>
+        <ul>
+            <li>Beavertown</li>
+            <li>BrewDog</li>
+            <li>Verdant</li>
+            <li>The Kernel</li>
+            <li>Cloudwater</li>
+            <li>Nort</li>
+        </ul>
+    </section>
+    <section class="">
+        <h2>Food.</h2>
+        <ul>
+            <li>Beavertown</li>
+            <li>BrewDog</li>
+            <li>Verdant</li>
+            <li>The Kernel</li>
+            <li>Cloudwater</li>
+            <li>Nort</li>
+        </ul>
+    </section>
+    <section class="">
+        <h2>Good Stuff.</h2>
+        <ul>
+            <li>Beavertown</li>
+            <li>BrewDog</li>
+            <li>Verdant</li>
+            <li>The Kernel</li>
+            <li>Cloudwater</li>
+            <li>Nort</li>
+        </ul>
+    </section>
+    <section class="">
+        <h2>Family.</h2>
+        <ul>
+            <li>Beavertown</li>
+            <li>BrewDog</li>
+            <li>Verdant</li>
+            <li>The Kernel</li>
+            <li>Cloudwater</li>
+            <li>Nort</li>
+        </ul>
+    </section>
+    <section class="">
+        <h2>Wellness.</h2>
+        <ul>
+            <li>Beavertown</li>
+            <li>BrewDog</li>
+            <li>Verdant</li>
+            <li>The Kernel</li>
+            <li>Cloudwater</li>
+            <li>Nort</li>
+        </ul>
+    </section>
 </div>
