262f6bc0d2a8ff7c589b43b743416818319a5bd7
Neil Lake <neil.1475369@gmail.com>
2020-02-24 17:55:25 +0000

    updates

--- site/templates/_text.php
+++ site/templates/_text.php
@@ -1,3 +1,3 @@
 <div class="a-designText">
-    Drinks.&nbsp;Music.&nbsp;Food.&nbsp;Friends.&nbsp;Family.&nbsp;Drinks.&nbsp;Music.&nbsp;Food.&nbsp;Friends.&nbsp;Family.
+    Drinks.&nbsp;Music.&nbsp;Food.&nbsp;Friends.&nbsp;Family.&nbsp;Drinks.&nbsp;Music.&nbsp;Food.&nbsp;Friends.&nbsp;Family.&nbsp;Drinks.&nbsp;Music.&nbsp;Food.&nbsp;Friends.&nbsp;Family.&nbsp;Drinks.&nbsp;Music.&nbsp;Food.&nbsp;Friends.&nbsp;Family.
 </div>
